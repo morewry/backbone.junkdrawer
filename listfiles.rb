29d950d01b0b80ae85826fcf060f188372a404db
Rachael L Moore <git@morewry.me>
2013-06-19 00:06:13 -0400

    move some more files

--- listfiles.rb
+++
@@ -1,8 +0,0 @@
-module Sass::Script::Functions
-    def listFiles(path)
-        return Sass::Script::List.new(
-            Dir.glob(path.value).map! { |x| Sass::Script::String.new(File.basename(x, '.*')) },
-            :comma
-        )
-    end
-end
