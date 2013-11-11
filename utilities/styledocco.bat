b75f3356a86a64d22d482455cf65838ac421b40c
Rachael L Moore <git@morewry.me>
2013-11-11 13:22:47 -0500

    update styledocco to generate from sass, not css

--- utilities/styledocco.bat
+++ utilities/styledocco.bat
@@ -4,7 +4,8 @@ set projdir=%~d0%~p0
 
 :start
 
-styledocco --name "" --out docs --include docs/preview.css css/all.css --verbose
+styledocco -n "" -o docs --include docs/preview.css --include css/all.css --preprocess
+or "scss --compass" css/sass --verbose
 
 shift
 if NOT x%1==x goto start
